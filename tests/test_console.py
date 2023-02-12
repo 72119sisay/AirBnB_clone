a6b6ee758c6fab156197622c4b602ca05a6a150a
72119sisay <sisayfikru69@gmail.com>
2023-02-12 04:46:12 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,10 +19,10 @@ from tests import clear_stream
 class TestHBNBCommand(unittest.TestCase):
     """Represents the test class for the HBNBCommand class.
     """
-     @unittest.skipIf(
-         os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
-     def test_fs_create(self):
-         """Tests the create command with the file storage.
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
+    def test_fs_create(self):
+        """Tests the create command with the file storage.
         """
         with patch('sys.stdout', new=StringIO()) as cout:
             cons = HBNBCommand()
@@ -32,20 +32,20 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertIn('City.{}'.format(mdl_id), storage.all().keys())
             cons.onecmd('show City {}'.format(mdl_id))
             self.assertIn("'name': 'Texas'", cout.getvalue().strip())
-             clear_stream(cout)
-             cons.onecmd('create User name="James" age=17 height=5.9')
-             mdl_id = cout.getvalue().strip()
-             self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
-             clear_stream(cout)
-             cons.onecmd('show User {}'.format(mdl_id))
-             self.assertIn("'name': 'James'", cout.getvalue().strip())
-             self.assertIn("'age': 17", cout.getvalue().strip())
-             self.assertIn("'height': 5.9", cout.getvalue().strip())
-             
+            clear_stream(cout)
+            cons.onecmd('create User name="James" age=17 height=5.9')
+            mdl_id = cout.getvalue().strip()
+            self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
+            clear_stream(cout)
+            cons.onecmd('show User {}'.format(mdl_id))
+            self.assertIn("'name': 'James'", cout.getvalue().strip())
+            self.assertIn("'age': 17", cout.getvalue().strip())
+            self.assertIn("'height': 5.9", cout.getvalue().strip())
+
     @unittest.skipIf(
-         os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
-     def test_db_create(self):
-          """Tests the create command with the database storage.
+        os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
+    def test_db_create(self):
+        """Tests the create command with the database storage.
         """
         with patch('sys.stdout', new=StringIO()) as cout:
             cons = HBNBCommand()
